bf67cd765dcf5d1230403e20db5cc9c239690979
Ploofyz <takeyukichan12@gmail.com>
2026-02-25 17:37:08 +0800

    Update ServerRanks.tsx
    Testing update code using "MYR\u00A012"

--- app/src/pages/ServerRanks.tsx
+++ app/src/pages/ServerRanks.tsx
@@ -30,7 +30,7 @@ const ranksData = [
     icon: <Star size={24} />,
     color: '#8FBACC',
     bgColor: 'rgba(143, 186, 204, 0.15)',
-    price: 'MYR 12',
+    price: 'MYR\u00A012',
     duration: '14d',
     durationType: 'temp',
     perks: [
